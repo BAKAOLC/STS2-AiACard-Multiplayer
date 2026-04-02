d1839c48d45402a98bfe9ed4b3033152a4d0be45
OLC <utsumabo@ritsukage.com>
2026-04-02 17:07:35 +0800

    refactor: Enhance MpDrainOthersDry card mechanics by adjusting damage, energy, and card draw values; improve localization descriptions for clarity in multiplayer interactions

--- Cards/Ironclad/MpDrainOthersDry.cs
+++ Cards/Ironclad/MpDrainOthersDry.cs
@@ -11,28 +11,71 @@ namespace STS2_AiACard_Multiplayer.Cards.Ironclad
     public sealed class MpDrainOthersDry()
         : MpOnlyModCardTemplate(0, CardType.Skill, CardRarity.Rare, TargetType.AnyAlly)
     {
+        private decimal _extraHpLossFromPlays;
+        private decimal _extraEnergyFromPlays;
+        private decimal _extraCardsFromPlays;
+
         protected override IEnumerable<DynamicVar> CanonicalVars =>
         [
             new DamageVar(6m, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move),
-            new HealVar(6m),
-            new DynamicVar("DrainTargetDraw", 0m),
+            new HealVar(4m),
+            new EnergyVar(1),
+            new CardsVar(1),
+            new DynamicVar("DrainRampHp", 3m),
+            new DynamicVar("DrainRampEnergy", 1m),
+            new DynamicVar("DrainRampCards", 1m),
         ];
+
+        public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
         public override CardAssetProfile AssetProfile => Const.PlaceholderCardArt;
 
         protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
         {
+            ArgumentNullException.ThrowIfNull(CombatState);
             var target = MpHelpers.RequireTargetPlayer(cardPlay);
             await MpHelpers.DealHpLoss(choiceContext, target.Creature, DynamicVars.Damage.BaseValue, this);
-            await PlayerCmd.GainEnergy(2, target);
+            await PlayerCmd.GainEnergy(DynamicVars.Energy.IntValue, target);
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.IntValue, target);
             await CreatureCmd.Heal(Owner.Creature, DynamicVars.Heal.BaseValue);
-            var draw = (int)DynamicVars["DrainTargetDraw"].BaseValue;
-            if (draw > 0) await CardPileCmd.Draw(choiceContext, draw, target);
+
+            var rh = DynamicVars["DrainRampHp"].BaseValue;
+            var re = DynamicVars["DrainRampEnergy"].BaseValue;
+            var rc = DynamicVars["DrainRampCards"].BaseValue;
+            DynamicVars.Damage.BaseValue += rh;
+            _extraHpLossFromPlays += rh;
+            DynamicVars.Energy.BaseValue += re;
+            _extraEnergyFromPlays += re;
+            DynamicVars.Cards.BaseValue += rc;
+            _extraCardsFromPlays += rc;
+
+            var copy = MpHelpers.CreateCard<MpDrainOthersDry>(CombatState, target, IsUpgraded);
+            copy.InheritScaledStatsFrom(this);
+            await MpHelpers.AddToHand(choiceContext, copy);
+        }
+
+        private void InheritScaledStatsFrom(MpDrainOthersDry source)
+        {
+            DynamicVars.Damage.BaseValue = source.DynamicVars.Damage.BaseValue;
+            DynamicVars.Energy.BaseValue = source.DynamicVars.Energy.BaseValue;
+            DynamicVars.Cards.BaseValue = source.DynamicVars.Cards.BaseValue;
+            _extraHpLossFromPlays = source._extraHpLossFromPlays;
+            _extraEnergyFromPlays = source._extraEnergyFromPlays;
+            _extraCardsFromPlays = source._extraCardsFromPlays;
         }
 
         protected override void OnUpgrade()
         {
-            DynamicVars["DrainTargetDraw"].UpgradeValueBy(2m);
+            DynamicVars.Energy.UpgradeValueBy(1m);
+            DynamicVars.Cards.UpgradeValueBy(1m);
+        }
+
+        protected override void AfterDowngraded()
+        {
+            base.AfterDowngraded();
+            DynamicVars.Damage.BaseValue += _extraHpLossFromPlays;
+            DynamicVars.Energy.BaseValue += _extraEnergyFromPlays;
+            DynamicVars.Cards.BaseValue += _extraCardsFromPlays;
         }
     }
 }
