acddb34b52e73f362aebed5fd58fd1f21737626b
OLC <utsumabo@ritsukage.com>
2026-04-04 18:24:11 +0800

    refactor: Update MpCorpseCurseToken mechanics to utilize dynamic damage values and enhance localization descriptions for improved clarity in multiplayer interactions

--- Cards/Necrobinder/MpCorpseCurseToken.cs
+++ Cards/Necrobinder/MpCorpseCurseToken.cs
@@ -1,15 +1,21 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using STS2RitsuLib.Scaffolding.Content;
 
 namespace STS2_AiACard_Multiplayer.Cards.Necrobinder
 {
-    /// <summary>尸语诅咒：回合结束时若在手中则受到 99 点伤害，随后将此牌消耗。</summary>
+    /// <summary>尸语诅咒：回合结束时若在手中则受到伤害，随后将此牌消耗。</summary>
     public sealed class MpCorpseCurseToken()
         : MpOnlyModCardTemplate(-1, CardType.Status, CardRarity.Status, TargetType.None, false)
     {
+        protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [
+            new DamageVar(99m, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move),
+        ];
+
         public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Unplayable];
 
         public override bool HasTurnEndInHandEffect => true;
@@ -18,7 +24,7 @@ namespace STS2_AiACard_Multiplayer.Cards.Necrobinder
 
         public override async Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
         {
-            await CreatureCmd.Damage(choiceContext, Owner.Creature, 99m,
+            await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.Damage.BaseValue,
                 ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, null, this);
             await CardCmd.Exhaust(choiceContext, this);
         }
